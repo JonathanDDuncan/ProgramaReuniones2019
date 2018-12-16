224f11ea34f9cbbe3b3a363104e41048efe5d0fc
Jonathan Duncan <duncanjonathan@yahoo.ca>
2018-12-16 08:04:28 -0600

    Have Main rely on App

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model, Msg(..), init, main, update, view)
+module Main exposing (main)
 
 import App
 import Browser
@@ -6,44 +6,6 @@ import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
 import LlenarSemana
 import Types.AppTypes
-
-
-
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!!!!11" ]
-        ]
 
 
 
