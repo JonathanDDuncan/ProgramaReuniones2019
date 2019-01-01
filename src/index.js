eadb03594ee8020bb317fc20e9d44b798c4a9a90
Jonathan Duncan <duncanjonathan@yahoo.ca>
2019-01-01 11:14:30 -0600

    Sort functions by category in index

--- src/index.js
+++ src/index.js
@@ -10,64 +10,35 @@ registerServiceWorker();
 $('.tabular.menu .item').tab();
 $('.ui.menu').find('.item').tab('change tab', 'tab-llenar')
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  var getstate = function () {
-    if (!window.jsApp)
-      window.jsApp = {};
-    if (!window.jsApp.state)
-      window.jsApp.state = {};
-    return window.jsApp.state;
-  }
+var getstate = function () {
+  if (!window.jsApp)
+    window.jsApp = {};
+  if (!window.jsApp.state)
+    window.jsApp.state = {};
+  return window.jsApp.state;
+}
 
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Run immediatetly on content load
   jsApp.load.loadCommonData(function (commonData) {
     var state = getstate();
     state.defaultsemana = commonData.defaultsemana;
     state.puntosMejoresLectoresMaestros = commonData.puntosMejoresLectoresMaestros;
   });
 
-
   jsApp.load.loadJSON('assets/json/publicadores.json', function (publicadores) {
     var state = getstate();
-    
-    state.publicadores = publicadores;
 
+    state.publicadores = publicadores;
     jsApp.tabulator.createtablesemanasllenar(state.publicadores)
   })
 
-  document.getElementById("download-json").addEventListener("click", function () {
-    tablesemanasllenar.download("json", "semanas para llenar " + moment().format() + ".json");
-
-  });
-
-  document.getElementById("process").addEventListener("click", function () {
-    var state = getstate();
-    state.semanasparallenar = jsApp.tabulators.tablesemanasllenar.getData();     
-    var semanastofill = jsApp.process.preprocesssemanas(state.semanasparallenar, state.defaultsemana, state.publicadores);
-
-    var timebetween = 100;
-    setTimeout(function () {
-      app.ports.clear.send("clear");
-      setTimeout(function () {
-        app.ports.loadCanciones.send(JSON.stringify(state.canciones));
-        setTimeout(function () {
-          app.ports.loadPublicadores.send(JSON.stringify(state.publicadores));
-          setTimeout(function () {
-            app.ports.loadSemanasAnteriores.send(JSON.stringify(state.semanasllenados));
-            setTimeout(function () {
-              app.ports.fillSemanas.send(JSON.stringify(semanastofill));
-            }, timebetween)
-          }, timebetween)
-        }, timebetween)
-      }, timebetween)
-    }, timebetween);
-  });
-
-
+  // Event listeners
   jsApp.load.loadfile('cargarcopiaseguridad', function (result) {
     if (result.publicadores && result.canciones && result.semanasllenados) {
 
       var state = getstate();
-      
+
       state.publicadores = result.publicadores;
       state.canciones = result.canciones;
       state.semanasllenados = result.semanasllenados;
@@ -90,6 +61,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
     $('.ui.menu').find('.item').tab('change tab', 'tab-llenar');
   });
 
+  document.getElementById("download-json").addEventListener("click", function () {
+    tablesemanasllenar.download("json", "semanas para llenar " + moment().format() + ".json");
+
+  });
+
+  document.getElementById("process").addEventListener("click", function () {
+    var state = getstate();
+    state.semanasparallenar = jsApp.tabulators.tablesemanasllenar.getData();
+    var semanastofill = jsApp.process.preprocesssemanas(state.semanasparallenar, state.defaultsemana, state.publicadores);
+
+    var timebetween = 100;
+    setTimeout(function () {
+      app.ports.clear.send("clear");
+      setTimeout(function () {
+        app.ports.loadCanciones.send(JSON.stringify(state.canciones));
+        setTimeout(function () {
+          app.ports.loadPublicadores.send(JSON.stringify(state.publicadores));
+          setTimeout(function () {
+            app.ports.loadSemanasAnteriores.send(JSON.stringify(state.semanasllenados));
+            setTimeout(function () {
+              app.ports.fillSemanas.send(JSON.stringify(semanastofill));
+            }, timebetween)
+          }, timebetween)
+        }, timebetween)
+      }, timebetween)
+    }, timebetween);
+  });
+
+
+  // Elm Ports
   app.ports.fillSemanasCallBack.subscribe(function (data) {
     console.log(JSON.stringify(data));
     jsApp.download.directDownloadJSON(data, "fillSemanas" + moment().format());
